8c18f413796098ae7d22d5dfb81084322d566c6a
Wayne Graham <wayne.graham@virginia.edu>
2015-08-04 09:37:43 -0400

    Update installer and add Readme
    Instructions for users
    move the binaries in to the ~/bin dir

---
+++ README.md
@@ -0,0 +1,45 @@
+# SARndbox Installer Script
+
+This script wraps the building of
+[SARndbox](https://github.com/KeckCAVES/SARndbox) and its depedencies. The one
+thing it does not The one dependency it does not install is
+[xquartz](http://xquartz.macosforge.org/landing/). You can install this
+via the disk image, or via Homebrew
+[Cask](https://github.com/caskroom/homebrew-cask).
+
+If you go the Cask route, you'll need to run these lines:
+
+```
+$ brew install caskroom/cask/brew-cask
+$ brew install xquartz
+```
+
+## Installing
+
+You'll just need to run the `installer.sh` script. This **should** walk
+you through everything that's going on on the system, including
+dowloading the source files, patching the `makefile` and moving the
+files to you `~/bin` directory.
+
+## Post-install
+To run SARndbox, execute the programs in `~/bin/`
+
+```
+$ ~/bin/
+```
+
+## Adding to PATH
+
+In the terminal, either set this for the session with:
+
+```
+$ export PATH=~/bin:$PATH
+```
+
+Or you can make it perminate by adding this to you `~/.bash_profile`
+
+```
+$ echo "PATH=~/bin:$PATH" >> ~/.bash_profile
+$ source ~/.bash_profile
+```
+
--- installer.sh
+++ installer.sh
@@ -1,5 +1,14 @@
 #! /usr/bin/env bash
 
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+
 unamestr=$(uname)
 
 if [[ "$unamestr" != 'Darwin' ]]; then
@@ -27,14 +36,9 @@ cd /tmp && tar xvf SARndbox-1.5-001.tar.gz
 cd /tmp && tar xvf Kinect-2.8-001.tar.gz
 
 # Update make file paths
-#VRUI_MAKEDIR to point to actual path
-vrui_makedir="\/usr/local\/Cellar\/vrui\/3.1-002-1\/share\/vrui\/make"
-
+# VRUI_MAKEDIR to point to actual path
 sed -i.bu 's/VRUI_MAKEDIR := $(HOME)\/Vrui-3.1\/share\/make/VRUI_MAKEDIR := \/usr\/local\/Cellar\/vrui\/3.1-002-1\/share\/vrui\/make/' /tmp/Kinect-2.8-001/makefile
 sed -i.bu 's/VRUI_MAKEDIR := $(HOME)\/Vrui-3.1\/share\/make/VRUI_MAKEDIR := \/usr\/local\/Cellar\/vrui\/3.1-002-1\/share\/vrui\/make/' /tmp/SARndbox-1.5-001/makefile
-
-#VRUI_MAKEDIR := $(HOME)/Vrui-3.1/share/make
-#sed -i "s/\$(HOME)\/Vrui-3.1\/share\/make /'VRUI_MAKEDIR := $vrui_makedir/" /tmp/Kinect-2.8-001/makefile
 
 echo "Building Kinect 3D Video Capture"
 cd /tmp/Kinect-2.8-001
@@ -43,3 +47,12 @@ make && make install
 echo "Building SARndbox..."
 cd /tmp/SARndbox-1.5-001
 make && make install
+
+echo "Moving files to ~/bin"
+mkdir -p ~/bin
+cd /tmp/SARndbox-1.5-001/bin
+chmod +x /tmp/SARndbox-1.5-001/bin
+mv * ~/bin
+
+echo "Make sure ~/bin is in your PATH"
+echo 'export PATH=~/bin:$PATH'
